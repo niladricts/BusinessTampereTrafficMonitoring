30e66056f09fe3fc32d1834f1dd61a8fe3d5fa61
Mikko <mikko77792@gmail.com>
2021-11-11 13:27:23 +0200

    replace original setup script with the updated one

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+### This shell script is used to setup environment for the system on a machine running Ubuntu. It:
+###  1. Installs database (if not already installed)
+###  2. Starts the database service (if not already running)
+###  4. Installs OpenCV
+###  5. Installs CUDA
+###  6. Installs Python 3.9
+###  7. Creates a virtual environment in env directory
+###  8. Installs required Python packages inside that virtual environment
+###  9. Downloads weights for the machine learning model
+###  10. Converts the weights to a format Tensorflow can understand
+
 # Safer bash scripting, see: https://explainshell.com/explain?cmd=set+-euxo+pipefail
 set -euxo pipefail
 
@@ -9,10 +20,19 @@ sudo apt update -y -qq
 ### Install some general tools
 sudo apt install -y software-properties-common git wget -qq
 
+### Database installation
+sudo apt install -y postgresql -qq
+
+### Start database service
+if sudo service postgresql status; then
+   echo "Database service is already running."
+else
+   echo "Database is not running. Starting..."
+   sudo service postgresql start
+fi
 
 ## OpenCV
 sudo apt install -y libopencv-dev -qq
-
 
 ## CUDA / cuDNN
 if ! dpkg -l libcudnn8; then
@@ -32,23 +52,13 @@ if ! dpkg -l libcudnn8; then
         libcudnn8-dev=${cudnn_version}-1+cuda11.2 -qq
 fi
 
-
-### Python installation check
-
-echo "Checking python installation..."
-if python3 -c 'import sys; exit(sys.version_info.major != 3 or sys.version_info.minor < 9)'; then
-    echo "Python 3.9+ already installed."
-else
-    sudo add-apt-repository -y ppa:deadsnakes/ppa
-    sudo apt install -y python3.9 python3.9-dev python3.9-venv -qq
-    echo "Installed Python 3.9."
-fi
-
+### Python installation
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt install -y python3.9 python3.9-dev python3.9-venv -qq
 
 ### Create virtual environment & install required Python packages inside
-python3.9 -m venv env
+test -f env/bin/pip || python3.9 -m venv env
 env/bin/pip install -r requirements.txt
-
 
 # Download weights for machine learning
 test -f yolov4.weights || wget -q "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights"
