2bd7dd500a1b50f026f70c14c406660df6b90fb4
Lassi Kirvesmäki <lassi.kirvesmaki@tuni.fi>
2021-11-12 16:53:36 +0200

    Some restructuring of the detect_by_lane_and_time function

--- BusinessTampereTrafficMonitoring/object_detector/object_detector.py
+++ BusinessTampereTrafficMonitoring/object_detector/object_detector.py
@@ -4,6 +4,7 @@ from collections import deque
 import cv2
 import dateutil.parser as dp
 import numpy as np
+import json
 from tf2_yolov4.anchors import YOLOV4_ANCHORS
 from tf2_yolov4.model import YOLOv4
 
@@ -41,36 +42,47 @@ def iso_to_unix(iso_time):
     return parsed_t.strftime('%s')
 
 
-def get_frame(time = time.time()):
-    return cache[find_nearest(timestamps, time)]
+def get_frame(timestamp=time.time()):
+    return cache[find_nearest(timestamps, timestamp)]
 
 
-def detect_by_lane_and_time(intersection, signal_group, ISO_timestamp, light_status):
+def filter_by_class(tuple):
+    if tuple[2] in ALLOWED_CLASSES and tuple[1] > 0:
+        return True
+    return False
+
+
+def lower_center_from_bbox(bbox):
+    return tuple((bbox[0]+bbox[2]) / 2, bbox[1])
+
+
+def detect_by_lane_and_time(intersection, lane, ISO_timestamp, light_status):
     """
     The bread and butter of the program:
         Function which can be called from the traffic lights API.
         Matches the given parameters to specific frames.
         Calls necessary utility functions to transform given parameters to usable formats.
-        Does operations on the frames to extract vehicle counts per lane and stores the count, timestamp and lane. 
+        Does operations on the frames to extract vehicle counts per lane and stores the count, timestamp and lane.
     """
     epoch_time = iso_to_unix(ISO_timestamp)
     frame_at_the_time = get_frame(epoch_time)
     prediction_frame = np.expand_dims(frame_at_the_time, axis=0) / 255.0
     boxes, scores, classes, detections = model.predict(prediction_frame)
+
     boxes = boxes[0] * [WIDTH, HEIGHT, WIDTH, HEIGHT]
     scores = scores[0]
     classes = classes[0].astype(int)
-    detections = detections[0]
-    vehicle_count = 0
-    for (xmin, ymin, xmax, ymax), score, class_idx in zip(boxes, scores, classes):
 
-        if (class_idx in ALLOWED_CLASSES) and score > 0:
-            object_center = (xmin, ymin)
-            # if point_inside(object_center, hull)
-            #   vehicle_count += 1
+    boxes, scores, classes = filter(filter_by_class, zip(boxes, scores, classes))
+    points = [lower_center_from_bbox(item) for item in boxes]
+    # vehicle_count = #needs implementing
+
+
+
+
 
     # TODO: Lane matching and updating database or cache with timestamp, lane and vehicle count
-
+    # data = '{"lane_id" : lane, "traffic_time" : epoch_time, "car_amount" : vehicle_count }
 
 def stream_reader(cap):
     """
@@ -91,6 +103,7 @@ timestamps = deque([])
 # TODO: Implementing multiple video captures depending on the intersection
 cap = cv2.VideoCapture('rtsp://rtsp.kvt.tampere.fi:55489/proxyStream')
 
+
 if not cap.isOpened():
     print('Cannot open stream')
     exit(-1)
@@ -99,6 +112,7 @@ if not cap.isOpened():
 ALLOWED_CLASSES = [1, 2, 3, 5, 7]
 WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+HULLS = dict()
 
 # Initializing model parameters
 model = YOLOv4(
