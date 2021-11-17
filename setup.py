b76478684d2a9543c3982d3296e50131219903aa
Mikko <mikko77792@gmail.com>
2021-11-17 20:12:25 +0200

    make package installable with pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="BusinessTampereTrafficMonitoring",
+    version="0.0.1",
+    url="https://github.com/niladricts/BusinessTampereTrafficMonitoring",
+    packages=find_packages()
+)
