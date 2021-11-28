093e0eb25fbf655548b8366e0264fc104b2204ef
Niladri Saha <niladri.saha@tuni.fi>
2021-11-28 21:52:59 +0200

    Doc string added

--- BusinessTampereTrafficMonitoring/traffic_lights/status.py
+++ BusinessTampereTrafficMonitoring/traffic_lights/status.py
@@ -44,7 +44,7 @@ class Status(Enum):
         http://wiki.itsfactory.fi/images/f/fe/DINT_GRINT_states.pdf
 		# Parameter:
 		# argument1: class with which the method associated with (cls object)
-		# argument12: status_str (String)
+		# argument2: status_str (String)
 		# Returns:
 		# Enum
         """
