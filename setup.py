72ef84007fcec8ef6b94cf686e97156b3e23b4bd
Mikko <Andriamanitra@users.noreply.github.com>
2021-12-18 20:50:14 +0000

    Use setuptools to install requirements (#30)
    * try to make installation nicer
    
    * update readme
    
    * remove old requirements.txt
    
    * Update github actions to work with new setup
    
    * show coverage statistics

--- setup.py
+++ setup.py
@@ -5,5 +5,16 @@ setup(
     name="BusinessTampereTrafficMonitoring",
     version="0.0.1",
     url="https://github.com/niladricts/BusinessTampereTrafficMonitoring",
+    install_requires=[
+        "iotticket@git+https://github.com/IoT-Ticket/IoTTicket-PythonLibrary@06330351ac874b93dfcd7784910c4b8c7d95be83",
+        "httpx",
+        "numpy",
+        "opencv-python",
+        "Pillow",
+        "respx",
+        "SQLAlchemy",
+        "tensorflow",
+        "tf2-yolov4",
+    ],
     packages=find_packages()
 )
